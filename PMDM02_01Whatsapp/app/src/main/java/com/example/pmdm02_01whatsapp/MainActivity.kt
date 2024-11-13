c0ac35c70278854a9d4ed5bad5dd02743ed3b932
JulioFertry <juliofertry@gmail.com>
2024-11-13 19:34:36 +0100

    inicio

---
+++ PMDM02_01Whatsapp/app/src/main/java/com/example/pmdm02_01whatsapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pmdm02_01whatsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pmdm02_01whatsapp.ui.theme.PMDM02_01WhatsappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PMDM02_01WhatsappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PMDM02_01WhatsappTheme {
+        Greeting("Android")
+    }
+}
