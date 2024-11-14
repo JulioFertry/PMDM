9727f8d60a28b3c678aaa66f5a0f7ed9f8866b2b
JulioFertry <juliofertry@gmail.com>
2024-11-14 18:33:50 +0100

    Chat

--- PMDM02_01Whatsapp/app/src/main/java/com/example/pmdm02_01whatsapp/MainActivity.kt
+++ PMDM02_01Whatsapp/app/src/main/java/com/example/pmdm02_01whatsapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,16 +14,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -92,6 +97,49 @@ fun TopBar(modifier: Modifier) {
         }
 
     }
+}
+
+
+@Composable
+fun Chat(profilePic: Painter, name: String, lastMessage: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 25.dp, vertical = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = profilePic,
+            contentDescription = "Foto de perfil",
+            modifier = Modifier.size(50.dp)
+                .clip(CircleShape)
+                .background(colorResource(id = R.color.dark_green))
+                .weight(1f)
+        )
+    }
+
+    Column(
+        modifier = Modifier.padding(start = 25.dp).fillMaxWidth()
+    ) {
+        Text(
+            text = name,
+            color = colorResource(id = R.color.black),
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = lastMessage,
+            color = colorResource(id = R.color.grey),
+            fontSize = 10.sp,
+            fontWeight = FontWeight.Light
+        )
+    }
+
+}
+
+
+@Composable
+fun ChatList() {
+
 }
 
 
