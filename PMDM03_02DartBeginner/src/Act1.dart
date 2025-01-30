78b6219bebf293cc7894719e10a8dc8acf4aecd2
JulioFertry <juliofertry@gmail.com>
2025-01-30 19:18:43 +0100

    divisores

--- PMDM03_02DartBeginner/src/Act1.dart
+++ PMDM03_02DartBeginner/src/Act1.dart
@@ -1,10 +1,11 @@
 import 'dart:core';
+import 'dart:ffi';
 import 'dart:io';
 
 class act1 {
 
   /** Pide un numero al usuario y lo devuelve en caso de que sea válido */
-  int? askForNumber() {
+  int askForNumber() {
     int number = 0;
 
     print('Introduce un número: ');
@@ -16,11 +17,24 @@ class act1 {
       throw Exception("El numero no puede ser nulo");
     }
 
+    if (number <= 0) {
+      throw Exception("El numero no puede ser 0 o menor");
+    }
+
     return number;
   }
 
-  int? calculateDividers(int) {
-    
+
+  /** Recibe un numero y devuelve una lista con todos sus divisores */
+  List<int> calculateDividers(number) {
+    List<int> dividers = [];
+    for (int i = number; i >= 1; i--) {
+      if (number % i == 0) {
+        dividers.add(i);
+      }
+    }
+    return dividers;
   }
+
 
 }
